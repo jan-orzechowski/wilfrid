b61c948d1bdd28c24af7149cd737796a41834662
jor <orzechowski.jan@gmail.com>
2022-10-28 20:24:10 +0200

    move stretchy buffers test to a separate function

--- main.c
+++ main.c
@@ -10,22 +10,23 @@ typedef struct buffer_header
     size_t len;
     size_t cap;
     char buf[0];
-} buffer_header;
+} 
+buffer_header;
 
 // offsetof zwraca pozycję pola w ramach structu
 // chodzi o to, że z buffer_header korzystamy tak, jak ze zwykłego void* 
 // ale potajemnie mamy zaalokowane zawsze trochę więcej, z przodu
-#define _buf_header(b) ((buffer_header*)((char*)(b) - offsetof(buffer_header, buf)))
-#define buf_len(b) ((b) ? _buf_header(b)->len : 0)
-#define buf_cap(b) ((b) ? _buf_header(b)->cap : 0)
+#define __buf_header(b) ((buffer_header*)((char*)(b) - offsetof(buffer_header, buf)))
+#define buf_len(b) ((b) ? __buf_header(b)->len : 0)
+#define buf_cap(b) ((b) ? __buf_header(b)->cap : 0)
 
-#define _buf_fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
-#define _buf_fit(b, n) (_buf_fits((b), (n)) ? 0 : ((b) = _buf_grow((b), buf_len(b) + (n), sizeof(*(b)))))
+#define __buf_fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
+#define __buf_fit(b, n) (__buf_fits((b), (n)) ? 0 : ((b) = __buf_grow((b), buf_len(b) + (n), sizeof(*(b)))))
 
-#define buf_push(b, x) (_buf_fit((b), 1), (b)[_buf_header(b)->len++] = (x))
-#define buf_free(b) ((b) ? (free(_buf_header(b)), (b) = NULL) : 0)
+#define buf_push(b, x) (__buf_fit((b), 1), (b)[__buf_header(b)->len++] = (x))
+#define buf_free(b) ((b) ? (free(__buf_header(b)), (b) = NULL) : 0)
 
-void* _buf_grow(const void* buf, size_t new_len, size_t elem_size)
+void* __buf_grow(const void* buf, size_t new_len, size_t elem_size)
 {
     size_t new_cap = max(1 + 2 * buf_cap(buf), new_len);
     assert(new_len <= new_cap);
@@ -33,7 +34,7 @@ void* _buf_grow(const void* buf, size_t new_len, size_t elem_size)
     buffer_header* new_hdr;
     if (buf)
     {
-        new_hdr = realloc(_buf_header(buf), new_size);
+        new_hdr = realloc(__buf_header(buf), new_size);
     }
     else
     {
@@ -44,26 +45,39 @@ void* _buf_grow(const void* buf, size_t new_len, size_t elem_size)
     return new_hdr->buf;
 }
 
-struct intern_str
+typedef struct intern_str
 {
     size_t len;
     char* ptr;
-};
+}
+intern_str;
 
-int main(int argc, char** argv)
+void stretchy_buffers_test()
 {
-    printf("hello\n");
-
-    struct intern_str* str = 0;
+    intern_str* str = 0;
 
     assert(buf_len(str) == 0);
 
     for (size_t index = 0; index < 1024; index++)
     {
-        buf_push(str, ((struct intern_str){ 1, NULL }));
+        buf_push(str, ((intern_str){ index, NULL }));
     }
 
     assert(buf_len(str) == 1024);
+
+    for (size_t index = 0; index < 1024; index++)
+    {
+        assert(str[index].len == index);
+    }
+
+    buf_free(str);
+
+    assert(buf_len(str) == 0);
+}
+
+int main(int argc, char** argv)
+{
+    stretchy_buffers_test();
 
     return 0;
 }
