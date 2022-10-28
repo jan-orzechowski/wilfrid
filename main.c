1722cc000018dd13376f81c30d6cc4eeca15ea31
jor <orzechowski.jan@gmail.com>
2022-10-28 21:04:44 +0200

    add basic token parsing

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#include <stddef.h>
+#include <ctype.h>
 
 typedef struct buffer_header
 {
@@ -25,6 +25,8 @@ buffer_header;
 
 #define buf_push(b, x) (__buf_fit((b), 1), (b)[__buf_header(b)->len++] = (x))
 #define buf_free(b) ((b) ? (free(__buf_header(b)), (b) = NULL) : 0)
+
+#define debug_breakpoint {int x = 0;}
 
 void* __buf_grow(const void* buf, size_t new_len, size_t elem_size)
 {
@@ -65,7 +67,7 @@ void stretchy_buffers_test()
 
     assert(buf_len(str) == 1024);
 
-    for (size_t index = 0; index < 1024; index++)
+    for (size_t index = 0; index < buf_len(str); index++)
     {
         assert(str[index].len == index);
     }
@@ -75,9 +77,155 @@ void stretchy_buffers_test()
     assert(buf_len(str) == 0);
 }
 
+typedef enum token_kind
+{
+    // pierwsze 128 wartości jest dla characters
+    TOKEN_LAST_CHAR = 127,
+    TOKEN_INT,
+    TOKEN_NAME,
+    // ...
+} 
+token_kind;
+
+typedef struct tok
+{
+    token_kind kind;
+    const char* start;
+    const char* end;
+    union
+    {
+        int val;
+        const char* name;
+    };
+}  
+tok;
+
+char* stream;
+tok token;
+tok** all_tokens;
+
+void next_token()
+{
+    token.start = stream;
+    switch (*stream)
+    {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        {
+            // idziemy po następnych
+            int val = 0;
+            while (isdigit(*stream))
+            {
+                val *= 10; // dotychczasową wartość traktujemy jako 10 razy większą - bo znaleźliśmy kolejne miejsce dziesiętne
+                val += *stream++ - '0'; // przerabiamy char na integer
+            }
+            token.kind = TOKEN_INT;
+            token.val = val;
+        }
+        break;
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'I':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'U':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+        case '_':
+        {
+            // zaczęliśmy od litery - dalej idziemy po cyfrach, literach i _
+            while (isalnum(*stream) || *stream == '_')
+            {
+                stream++;
+            }
+            token.kind = TOKEN_NAME;
+            token.name = token.start;
+        }
+        break;
+        default:
+        {
+            token.kind = *stream++;
+        }
+        break;
+    }
+
+    token.end = stream;
+    stream++; 
+
+    tok* new_tok = malloc(sizeof(token));
+    memcpy(new_tok, &token, sizeof(token));
+    buf_push(all_tokens, new_tok); 
+}
+
+void init_stream(const char* str)
+{
+    stream = str;
+    next_token();
+}
+
 int main(int argc, char** argv)
 {
     stretchy_buffers_test();
+
+    const char* parsing_test = "AA 12A BB A21 CC";
+
+    init_stream(parsing_test);
+    while (token.kind)
+    {
+        next_token();
+    }
 
     return 0;
 }
