e4b39a19ba0dfb946a3a9ae421883d08accb9411
jor <orzechowski.jan@gmail.com>
2023-01-27 14:24:37 +0100

    move file io functions to utils

--- utils.h
+++ utils.h
@@ -661,3 +661,68 @@ void map_add_to_chain(chained_hashmap* map, void* key, void* value)
         map->total_count++;
     }
 }
+
+typedef struct string_ref
+{
+    char* str;
+    size_t length;
+} string_ref;
+
+
+string_ref read_file(char* filename)
+{
+    FILE* src;
+    errno_t err = fopen_s(&src, filename, "rb");
+    if (err != 0)
+    {
+        // błąd
+        return (string_ref) { 0 };
+    }
+
+    fseek(src, 0, SEEK_END);
+    size_t size = ftell(src);
+    fseek(src, 0, SEEK_SET);
+
+    if (size > 0)
+    {
+        char* str_buf = xmalloc(size + 1);
+        str_buf[size] = 0; // null terminator
+
+        size_t objects_read = fread(str_buf, size, 1, src);
+        if (objects_read == 1)
+        {
+            fclose(src);
+            return (string_ref) { .str = str_buf, .length = size };
+        }
+        else
+        {
+            fclose(src);
+            free(str_buf);
+            return (string_ref) { 0 };
+        }
+    }
+    else
+    {
+        fclose(src);
+        return (string_ref) { 0 };
+    }
+}
+
+bool write_file(const char* path, const char* buf, size_t len)
+{
+    FILE* file;
+    errno_t err = fopen_s(&file, path, "w");
+    if (err != 0)
+    {
+        // błąd
+        return false;
+    }
+
+    if (!file)
+    {
+        return false;
+    }
+    size_t elements_written = fwrite(buf, len, 1, file);
+    fclose(file);
+    return (elements_written == 1);
+}
