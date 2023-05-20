c57eb6579d02e340135ee4fa9be14ba002a344e4
jor <orzechowski.jan@gmail.com>
2023-05-20 12:22:10 +0200

    update Emscripten builds

--- emscripten_debug_build.bat
+++ emscripten_debug_build.bat
@@ -3,16 +3,17 @@ call emsdk_env.bat
 
 cd /d D:\JO_Programowanie\Projekty\CompilerProject
 if not exist wasm\debug mkdir wasm\debug
+del wasm\debug /Q
 
-set disable-warnings=-Wno-switch -Wno-unused-value -Wno-unused-variable -Wno-unused-but-set-variable -Wno-sign-compare -Wno-typedef-redefinition -Wno-format-security -Wno-incompatible-pointer-types-discards-qualifiers
+set disable-warnings=-Wno-switch -Wno-unused-value -Wno-unused-variable -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-sign-compare -Wno-typedef-redefinition -Wno-format-security -Wno-incompatible-pointer-types-discards-qualifiers -Wno-limited-postlink-optimizations
 
-set debug-flags=-DDEBUG_BUILD -sASSERTIONS=1 
+set warnings-options=-Wall -Wextra %disable-warnings%
 
-set flags=-D_DEFAULT_SOURCE -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=128MB -sTOTAL_STACK=32MB -sNO_EXIT_RUNTIME=1 -sINVOKE_RUN=0 -sEXPORTED_FUNCTIONS="['_main']" -sEXPORTED_RUNTIME_METHODS="['ccall', 'callMain']" --preload-file test
+set general-options=-D_DEFAULT_SOURCE -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=48MB -sTOTAL_STACK=2MB -sNO_EXIT_RUNTIME=1 -sINVOKE_RUN=0 -sEXPORTED_FUNCTIONS="['_main', '_compile_input']" -sEXPORTED_RUNTIME_METHODS="['ccall', 'callMain']"
 
-set output-options=-o wasm/debug/index.html -O0 -g3 -gsource-map --source-map-base http://localhost:6931/ --emrun --memoryprofiler --shell-file emscripten_shell.html
+set debug-options=-DDEBUG_BUILD -sASSERTIONS=2 -o wasm/debug/index.html -O0 -g3 -gsource-map --source-map-base http://localhost:6931/ --emrun --memoryprofiler --shell-file emscripten_debug_shell.html
 
-call emcc -std=c99 -Wall -Wextra %flags% %debug-flags% %disable-warnings% main.c %output-options%
+call emcc -std=c99 %warnings-options% %general-options% %debug-options% main.c 
 
 pause
 
--- emscripten_release_build.bat
+++ emscripten_release_build.bat
@@ -3,10 +3,19 @@ call emsdk_env.bat
 
 cd /d D:\JO_Programowanie\Projekty\CompilerProject
 if not exist wasm\release mkdir wasm\release
+del wasm\release /Q
 
-set disable-warnings=-Wno-switch -Wno-unused-value -Wno-typedef-redefinition -Wno-format-security -Wno-incompatible-pointer-types-discards-qualifiers
+set disable-warnings=-Wno-switch -Wno-unused-value -Wno-unused-variable -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-sign-compare -Wno-typedef-redefinition -Wno-format-security -Wno-incompatible-pointer-types-discards-qualifiers
 
-call emcc -std=c99 -D_DEFAULT_SOURCE %disable-warnings% main.c -o wasm/release/index.html --shell-file emscripten_shell.html
+set warnings-options=-Wall -Wextra %disable-warnings%
 
-del "wasm\release\lang.zip"
-"C:\Program Files\7-Zip\7z.exe" a -tzip "wasm\release\lang.zip" %CD%\wasm\release\*
+set general-options=-D_DEFAULT_SOURCE -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=48MB -sTOTAL_STACK=2MB -sNO_EXIT_RUNTIME=1 -sINVOKE_RUN=0 -sEXPORTED_FUNCTIONS="['_main', '_compile_input']" -sEXPORTED_RUNTIME_METHODS="['ccall', 'callMain']" --preload-file test
+
+set release-options=-sSAFE_HEAP=0 -sASSERTIONS=0 -o wasm/release/nous.js -O3
+
+call emcc -std=c99 %warnings-options% %general-options% %release-options% main.c 
+
+pause
+
+del C:\wamp64\www\nous /Q
+xcopy /s "wasm\release" C:\wamp64\www\nous
